c3376477eeec53404b8b694938d28f685451d23e
Andrew Sorensen <andrew@localcoast.net>
2013-03-19 14:48:23 -0700

    Initial version of virtual machine pusher

---
+++ push-vms.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# the location of the virtual machines to push
+SOURCE_LOCATION=/home/$USER/vms/
+DESTINATION_LOCATION=/home/$USER/vms/
+
+MACHINE_PREFIX="cn"
+STARTING_INDEX=2
+ENDING_INDEX=12
+
+trap 'echo " Quiting on signal. was deploying to machine $MACHINE_PREFIX$i"; exit' SIGINT SIGTERM
+
+for i in `seq $STARTING_INDEX $ENDING_INDEX`; do
+    rsync -rvh $SOURCE_LOCATION $MACHINE_PREFIX$i:$DESTINATION_LOCATION -P
+done
