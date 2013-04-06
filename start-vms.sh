611aab3b174bb160b29373d750e342bc0593c50c
Andrew Sorensen <andrew@localcoast.net>
2013-04-05 19:10:31 -0700

    Add script to start VMs

---
+++ start-vms.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+MACHINE_PREFIX="cn"
+STARTING_INDEX=2
+ENDING_INDEX=12
+
+trap 'echo " Quiting on signal. was working on machine $MACHINE_PREFIX$i"; exit' SIGINT SIGTERM
+
+for i in `seq $STARTING_INDEX $ENDING_INDEX`; do
+    ssh -f $MACHINE_PREFIX$i "export DISPLAY=:1 VBoxManage startvm $1"
+done
